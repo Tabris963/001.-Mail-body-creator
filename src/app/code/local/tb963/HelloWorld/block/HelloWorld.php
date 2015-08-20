9bbcd430d649265dc606331a37e80608e76202cb
Riccardo Zenere <tabris963@gmail.com>
2015-08-20 16:58:58 +0200

    Created basic structure for 'HelloWorld' testing

---
+++ src/app/code/local/tb963/HelloWorld/block/HelloWorld.php
@@ -0,0 +1,4 @@
+class test_HelloWorld_Block_HelloWorld extends Mage_Core_Block_Template
+{
+  // necessary methods
+}
---
+++ src/app/code/local/tb963/HelloWorld/controllers/IndexController.php
@@ -0,0 +1,8 @@
+class test_HelloWorld_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+     $this->loadLayout(array('default'));
+     $this->renderLayout();
+    }
+}
